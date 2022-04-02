5966347b9a0f357ad19bc6c056cfcdfaf13b77fe
Jack Whitham <jack.d.whitham@gmail.com>
2022-04-02 15:12:00 +0100

    how can I know that CK is a bad move

--- triangle.py
+++ triangle.py
@@ -17,14 +17,22 @@ class Cell:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
-        self.met = False
-        self.busy = False
+        self.met_stack: typing.List[bool] = [False]
+        self.busy_stack: typing.List[bool] = [False]
 
     def copy(self) -> "Cell":
         c = Cell(self.x, self.y)
-        c.met = self.met
-        c.busy = self.busy
+        c.met_stack = self.met_stack[:]
+        c.busy_stack = self.busy_stack[:]
         return c
+
+    def push(self) -> None:
+        self.met_stack.append(self.met_stack[-1])
+        self.busy_stack.append(self.busy_stack[-1])
+
+    def pop(self) -> None:
+        self.met_stack.pop()
+        self.busy_stack.pop()
 
 class Grid:
     def __init__(self, num_people: int) -> None:
@@ -36,15 +44,25 @@ class Grid:
 
     def copy(self) -> "Grid":
         g = Grid(self.num_people)
-        for x in range(1, num_people):
+        for x in range(1, self.num_people):
             for y in range(x):
                 g.grid[(x, y)] = self.grid[(x, y)].copy()
         return g
 
+    def push(self) -> None:
+        for x in range(1, self.num_people):
+            for y in range(x):
+                self.grid[(x, y)].push()
+
+    def pop(self) -> None:
+        for x in range(1, self.num_people):
+            for y in range(x):
+                self.grid[(x, y)].pop()
+
     def reset_busy(self) -> None:
         for x in range(1, self.num_people):
             for y in range(x):
-                self.grid[(x, y)].busy = self.grid[(x, y)].met
+                self.grid[(x, y)].busy_stack = [self.grid[(x, y)].met_stack[-1]]
 
     def visit_related(self, cr: int, mark_busy: bool) -> int:
         assert 0 <= cr < self.num_people
@@ -52,17 +70,17 @@ class Grid:
 
         # Column cr, downwards (height of column cr is cr)
         for i in range(cr):
-            if not self.grid[(cr, i)].busy:
+            if not self.grid[(cr, i)].busy_stack[-1]:
                 count += 1
             if mark_busy:
-                self.grid[(cr, i)].busy = True
+                self.grid[(cr, i)].busy_stack[-1] = True
 
         # Row y, across
         for i in range(cr + 1, self.num_people):
-            if not self.grid[(i, cr)].busy:
+            if not self.grid[(i, cr)].busy_stack[-1]:
                 count += 1
             if mark_busy:
-                self.grid[(i, cr)].busy = True
+                self.grid[(i, cr)].busy_stack[-1] = True
 
         return count
 
@@ -79,7 +97,7 @@ class Grid:
         assert 0 <= y < x < self.num_people
         self.visit_related(x, True)
         self.visit_related(y, True)
-        self.grid[(x, y)].met = True
+        self.grid[(x, y)].met_stack[-1] = True
 
     def __str__(self) -> None:
         out: typing.List[str] = []
@@ -95,9 +113,9 @@ class Grid:
             for x in range(1, self.num_people):
                 if y >= x:
                     out.append(blank)
-                elif self.grid[(x, y)].met:
+                elif self.grid[(x, y)].met_stack[-1]:
                     out.append("met".center(w))
-                elif self.grid[(x, y)].busy:
+                elif self.grid[(x, y)].busy_stack[-1]:
                     out.append("bsy".center(w))
                 else:
                     (a, b) = self.count_not_busy(x, y)
@@ -107,17 +125,91 @@ class Grid:
 
         return "".join(out)
 
-    def find_all_available(self) -> Pairs:
+    def find_all_available(self, debug: bool, remaining: int) -> Pairs:
         available: typing.List[typing.Tuple[int, int, int]] = []
         for y in range(self.num_people - 1):
             for x in range(y + 1, self.num_people):
-                if not self.grid[(x, y)].busy:
-                    assert not self.grid[(x, y)].met
+                if not self.grid[(x, y)].busy_stack[-1]:
+                    assert not self.grid[(x, y)].met_stack[-1]
                     value = self.count_not_busy(x, y)
                     available.append((value, x, y))
 
-        available.sort()
+        if len(available) != 0:
+            available.sort()
+
+            #(match, x, y) = available[0]
+            #for i in range(len(available)):
+            #    (score, x, y) = available[i]
+            #    if score != match:
+            #        available = available[:i]
+            #        break
+
+            if debug:
+                indent = (self.num_people // 2) - remaining
+                print(" " * indent, "remaining", remaining)
+                for y in range(self.num_people):
+                    a = self.visit_related(y, False)
+                    if a != 0:
+                        print(" " * indent, "visit_related", name_to_string(y), a)
+                for (score, x, y) in available:
+                    print(" " * indent, "choice", score, pairs_to_string([(x, y)]))
         return [(x, y) for (_, x, y) in available]
+
+    def find_pairs(self, debug: bool) -> Pairs:
+        self.reset_busy()
+
+        if debug:
+            print(str(self))
+
+        (bt, pairs) = self.find_some_pairs(debug, self.num_people // 2)
+        #print ("backtrack", bt, flush=True)
+
+        for p in pairs:
+            (x, y) = p
+            self.set_met(x, y)
+
+        return pairs
+
+    def find_some_pairs(self, debug: bool, remaining: int) -> typing.Tuple[int, Pairs]:
+        if remaining == 0:
+            return (0, [])
+
+        assert remaining > 0
+        backtrack = 0
+        indent = (self.num_people // 2) - remaining
+        bad = True
+
+        for p in self.find_all_available(debug, remaining):
+            (x, y) = p
+
+            if debug:
+                print(" " * indent, "push", pairs_to_string([p]))
+
+            self.push()
+            self.set_met(x, y)
+            (bt, pairs) = self.find_some_pairs(debug, remaining - 1)
+            backtrack += bt
+            if len(pairs) == (remaining - 1):
+                pairs.append(p)
+                if debug:
+                    print(" " * indent, "solved", pairs_to_string([p]))
+
+                return (backtrack, pairs)
+
+            if debug:
+                print(" " * indent, "pop", pairs_to_string([p]))
+            else:
+                raise CantSolveError()
+
+            backtrack += 1
+            bad = False
+            self.pop()
+
+        if bad and debug:
+            print(" " * indent, "nowhere to go, with", remaining, "left")
+
+        return (backtrack, [])
+
 
     def can_solve(self, pairs: typing.List[Pairs]) -> bool:
         target_pairs = self.num_people // 2
@@ -133,7 +225,7 @@ class Grid:
         already_met = set()
         for x in range(1, self.num_people):
             for y in range(x):
-                if self.grid[(x, y)].met:
+                if self.grid[(x, y)].met_stack[-1]:
                     assert y < x
                     already_met.add((y, x))
 
@@ -214,43 +306,10 @@ def pairs_to_string(pairs: Pairs) -> str:
         out.append(" ")
     return "".join(out)
 
-def find_pairs(grid: Grid, debug: bool) -> Pairs:
-    grid.reset_busy()
-    (bt, pairs) = find_some_pairs(grid, debug, grid.num_people // 2)
-    print ("backtrack", bt, flush=True)
-
-    for p in pairs:
-        (x, y) = p
-        grid.set_met(x, y)
-
-    return pairs
-
-def find_some_pairs(grid: Grid, debug: bool, remaining: int) -> typing.Tuple[int, Pairs]:
-    if remaining == 0:
-        return (0, [])
-
-    assert remaining > 0
-    backtrack = 0
-
-    for p in grid.find_all_available():
-        (x, y) = p
-        copy_grid = grid.copy()
-        copy_grid.set_met(x, y)
-        (bt, pairs) = find_some_pairs(copy_grid, debug, remaining - 1)
-        backtrack += bt
-        if len(pairs) == (remaining - 1):
-            pairs.append(p)
-            return (backtrack, pairs)
-
-        backtrack += 1
-
-
-    return (backtrack, [])
-
-
 def test(num_people: int, debug: bool) -> None:
     g = Grid(num_people)
     met = set()
+    stop = False
     for i in range(num_people - 1):
         if debug:
             print("")
@@ -258,17 +317,18 @@ def test(num_people: int, debug: bool) -> None:
         busy = [False for i in range(num_people)]
         g2 = g.copy()
         try:
-            pairs = find_pairs(g, debug)
+            pairs = g.find_pairs(debug)
         except CantSolveError:
             debug = True
             g = g2.copy()
             try:
-                pairs = find_pairs(g, debug)
+                pairs = g.find_pairs(debug)
+                stop = True
             except CantSolveError as e:
                 raise e from None
                 
 
-        print("round {}: {}".format(i, pairs_to_string(pairs)))
+        print("round {}: {}".format(i, pairs_to_string(pairs)), flush=True)
         assert len(pairs) == (num_people // 2)
         for (x, y) in pairs:
             assert 0 <= y < x < num_people
@@ -278,6 +338,9 @@ def test(num_people: int, debug: bool) -> None:
             met.add((x, y))
             busy[x] = True
             busy[y] = True
+        if stop:
+            raise CantSolveError()
+
 
     for x in range(1, num_people):
         for y in range(x):
@@ -286,6 +349,6 @@ def test(num_people: int, debug: bool) -> None:
 
     assert len(met) == 0
 
-for num_people in range(4, 100, 2):
-    print("number of people = {}".format(num_people), flush=True)
-    test(num_people, False)
+for np in range(4, 100, 2):
+    print("number of people = {}".format(np), flush=True)
+    test(np, False)
